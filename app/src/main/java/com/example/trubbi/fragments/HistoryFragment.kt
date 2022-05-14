6dddece155a9013ba83cea4cdc47c134e34f9862
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-05-14 10:26:41 -0300

    Historial and Agendados fragments fixed

--- app/src/main/java/com/example/trubbi/fragments/HistoryFragment.kt
+++ app/src/main/java/com/example/trubbi/fragments/HistoryFragment.kt
@@ -6,25 +6,43 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.trubbi.R
-import com.example.trubbi.adapters.EventDataAdapter
-import com.example.trubbi.classes.HistFavSchedFragmentViewModel
-import com.example.trubbi.providers.EventsProvider
+import com.example.trubbi.adapters.EventListAdapter
+import com.example.trubbi.entities.Event
+import com.google.android.material.snackbar.Snackbar
 
 class HistoryFragment : Fragment(),LifecycleOwner {
 
     private lateinit var thisView:View
     private lateinit var recycler:RecyclerView
-    private lateinit var viewModel:HistFavSchedFragmentViewModel
+    var events : MutableList<Event> = ArrayList<Event>()
+    private lateinit var linearLayoutManager: LinearLayoutManager
+    private lateinit var eventListAdapter: EventListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
 
-        viewModel = ViewModelProvider(this).get(HistFavSchedFragmentViewModel::class.java)
+    override fun onStart() {
+        super.onStart()
+        for (i in 1..5){
+            events.add(Event("Evento al que asistí $i", "21-12-22", "12hs","Este evento es para toda la famiilia y niños ... Leer más...", "Vte. López", "https://picsum.photos/150?random=8"))
+            events.add(Event("Evento al que asistí $i", "21-12-22", "12hs","Titeres y comida tradicional argentina, case ... Leer más...", "Vte. López", "https://picsum.photos/150?random=3"))
+            events.add(Event("Evento al que asistí $i", "21-12-22", "12hs","Divertirte! Show gratuito de los Palmeras en ... Leer más...", "Vte. López", "https://picsum.photos/150?random=5"))
+            events.add(Event("Evento al que asistí $i", "21-12-22", "12hs","Cine al aire libre y gratuito, comidas y más ... Leer más...", "Vte. López", "https://picsum.photos/150?random=1"))
+            events.add(Event("Evento al que asistí $i", "21-12-22", "12hs","Feria artesanal, con show de malabares y una ... Leer más...", "Vte. López", "https://picsum.photos/150?random=7"))
+            events.add(Event("Evento al que asistí $i", "21-12-22", "12hs","Torneo de Voley, inscripción abierta, hasta  ... Leer más...", "Vte. López", "https://picsum.photos/150?random=9"))
+        }
+        recycler.setHasFixedSize(true)
+        linearLayoutManager = LinearLayoutManager(context)
+        recycler.layoutManager = linearLayoutManager
 
+        eventListAdapter = EventListAdapter(events){
+                x -> onItemClick(x)
+        }
+        recycler.adapter = eventListAdapter
     }
 
     override fun onCreateView(
@@ -34,21 +52,11 @@ class HistoryFragment : Fragment(),LifecycleOwner {
         // Inflate the layout for this fragment
         thisView = inflater.inflate(R.layout.fragment_history, container, false)
         recycler = thisView.findViewById(R.id.historyRecycler)
-        initRecyclerView()
         return thisView
     }
 
-    private fun initRecyclerView(){
-        val recyclerView = thisView.findViewById<RecyclerView>(R.id.historyRecycler)
-        recyclerView.layoutManager = LinearLayoutManager(context)
-
-        recyclerView.adapter = EventDataAdapter(EventsProvider.historyDataList)
-
+    private fun onItemClick(position: Int):Boolean{
+        Snackbar.make(thisView, position.toString(), Snackbar.LENGTH_SHORT).show()
+        return true
     }
-
-
-
-
-
-
 }
--- app/src/main/java/com/example/trubbi/fragments/MyEventsFragment.kt
+++ app/src/main/java/com/example/trubbi/fragments/MyEventsFragment.kt
@@ -8,36 +8,54 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.trubbi.R
-import com.example.trubbi.adapters.EventDataAdapter
-import com.example.trubbi.providers.EventsProvider
+import com.example.trubbi.adapters.EventListAdapter
+import com.example.trubbi.entities.Event
+import com.google.android.material.snackbar.Snackbar
 
 class MyEventsFragment : Fragment() {
 
     private lateinit var thisView:View
-    private lateinit var recycler: RecyclerView
+    private lateinit var recycler:RecyclerView
+    var events : MutableList<Event> = ArrayList<Event>()
+    private lateinit var linearLayoutManager: LinearLayoutManager
+    private lateinit var eventListAdapter: EventListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
 
+    override fun onStart() {
+        super.onStart()
+        for (i in 1..5){
+            events.add(Event("Mi Evento.$i", "21-12-22", "12hs","Este evento es para toda la famiilia y niños ... Leer más...", "Vte. López", "https://picsum.photos/150?random=8"))
+            events.add(Event("Mi Evento.$i", "21-12-22", "12hs","Titeres y comida tradicional argentina, case ... Leer más...", "Vte. López", "https://picsum.photos/150?random=3"))
+            events.add(Event("Mi Evento.$i", "21-12-22", "12hs","Divertirte! Show gratuito de los Palmeras en ... Leer más...", "Vte. López", "https://picsum.photos/150?random=5"))
+            events.add(Event("Mi Evento.$i", "21-12-22", "12hs","Cine al aire libre y gratuito, comidas y más ... Leer más...", "Vte. López", "https://picsum.photos/150?random=1"))
+            events.add(Event("Mi Evento.$i", "21-12-22", "12hs","Feria artesanal, con show de malabares y una ... Leer más...", "Vte. López", "https://picsum.photos/150?random=7"))
+            events.add(Event("Mi Evento.$i", "21-12-22", "12hs","Torneo de Voley, inscripción abierta, hasta  ... Leer más...", "Vte. López", "https://picsum.photos/150?random=9"))
+        }
+        recycler.setHasFixedSize(true)
+        linearLayoutManager = LinearLayoutManager(context)
+        recycler.layoutManager = linearLayoutManager
 
+        eventListAdapter = EventListAdapter(events){
+                x -> onItemClick(x)
+        }
+        recycler.adapter = eventListAdapter
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // Inflate the layout for this fragment
         thisView = inflater.inflate(R.layout.fragment_my_events, container, false)
         recycler = thisView.findViewById(R.id.myEventsRecycler)
-        initRecyclerView()
-        // Inflate the layout for this fragment
         return thisView
     }
 
-    private fun initRecyclerView(){
-        val recyclerView = thisView.findViewById<RecyclerView>(R.id.myEventsRecycler)
-        recyclerView.layoutManager = LinearLayoutManager(context)
-
-        recyclerView.adapter = EventDataAdapter(EventsProvider.myEventsDataList)
-
+    private fun onItemClick(position: Int):Boolean{
+        Snackbar.make(thisView, position.toString(), Snackbar.LENGTH_SHORT).show()
+        return true
     }
 }
