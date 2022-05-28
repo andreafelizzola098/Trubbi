411929d3682fff916e833d18c973b7eb97d1acc4
asfelizzola <asfelizzola@gmail.com>
2022-05-28 16:28:13 -0300

    searchView Fragment

---
+++ app/src/main/java/com/example/trubbi/fragments/SearchFragment.kt
@@ -0,0 +1,29 @@
+package com.example.trubbi.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.trubbi.R
+
+class SearchFragment : Fragment() {
+
+    private lateinit var searchV : View
+    private lateinit var recyclerSearch : RecyclerView
+    private lateinit var msgSearch : TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        searchV = inflater.inflate(R.layout.fragment_search, container, false)
+        recyclerSearch = searchV.findViewById(R.id.recyclerViewSearch)
+        msgSearch = searchV.findViewById(R.id.searchMsg)
+        return searchV
+    }
+}
