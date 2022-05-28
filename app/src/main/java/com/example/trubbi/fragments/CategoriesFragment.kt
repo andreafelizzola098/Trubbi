3c34d001e7b4b080a7b6737178e614ac39777b38
asfelizzola <asfelizzola@gmail.com>
2022-05-28 11:56:53 -0300

    hide the searchView

--- app/src/main/java/com/example/trubbi/fragments/CategoriesFragment.kt
+++ app/src/main/java/com/example/trubbi/fragments/CategoriesFragment.kt
@@ -7,9 +7,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.trubbi.R
+import com.example.trubbi.activities.MainActivity
 import com.example.trubbi.adapters.EventListAdapter
 import com.example.trubbi.entities.Event
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
@@ -24,22 +26,29 @@ class CategoriesFragment : Fragment() {
     private lateinit var eventListAdapter: EventListAdapter
     private lateinit var extendedFabCategory : Button
     private lateinit var titlecategory : TextView
+    private lateinit var toolBarSearchView: View
+
     val items_categories = arrayOf("Artes Escénicas", "Arte y Cultura", "Deportes", "Familia y Niños", "Ferias y Conferencias", "Música", "Otros", "Cercanos")
 
-
-    //override fun onCreate(savedInstanceState: Bundle?) {
-    //    super.onCreate(savedInstanceState)
-    //}
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        toolBarSearchView = requireActivity().findViewById(R.id.searchView)
+        toolBarSearchView.isVisible = false
+        if(activity != null){
+            (activity as MainActivity).supportActionBar?.title = "Categorias"
+        }
         category_view = inflater.inflate(R.layout.fragment_categories, container, false)
         categoryRecyclerView = category_view.findViewById(R.id.recycler_view_categories)
         extendedFabCategory = category_view.findViewById(R.id.extended_fab_category)
-        titlecategory = category_view.findViewById(R.id.textcategory)
+        //titlecategory = category_view.findViewById(R.id.textcategory)
+
         return category_view
     }
 
@@ -58,7 +67,7 @@ class CategoriesFragment : Fragment() {
         linearLayoutManager = LinearLayoutManager(context)
         categoryRecyclerView.layoutManager = linearLayoutManager
         var text = CategoriesFragmentArgs.fromBundle(requireArguments()).namecategory
-        titlecategory?.setText(text.toString())
+        //titlecategory?.setText(text.toString())
         //titlecategory.elevation = 10.0F
         eventListAdapter = EventListAdapter(events){
                 x -> onItemClick(x)
@@ -81,6 +90,11 @@ class CategoriesFragment : Fragment() {
     fun onItemClick(position: Int):Boolean{
         Snackbar.make(category_view, position.toString(), Snackbar.LENGTH_SHORT).show()
         return true
+    }
+
+    override fun onStop() {
+        super.onStop()
+        toolBarSearchView.isVisible = true
     }
 
 }
